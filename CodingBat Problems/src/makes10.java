0238a4be8485c5a59877f2b97d66d37eb7d1efd7
Omair Al-Falahi <3mairalfala7i33@gmail.com>
2025-07-27 11:57:44 +0400

    warmup1-makes10

---
+++ CodingBat Problems/src/makes10.java
@@ -0,0 +1,13 @@
+public class makes10 {
+
+    public boolean makes10(int a, int b) {
+        return (a == 10 || b == 10 || a + b == 10);
+    }
+
+    public static void main(String[] args) {
+        makes10 obj = new makes10();
+        System.out.println(obj.makes10(9, 10)); 
+        System.out.println(obj.makes10(9, 9));
+        System.out.println(obj.makes10(1, 9));
+    }
+}
