c57ec7b10f24c295415c24b1c9d3bdfb4e630815
Omair Al-Falahi <3mairalfala7i33@gmail.com>
2025-07-27 13:09:19 +0400

    warmup1-or35

---
+++ CodingBat Problems/src/or35.java
@@ -0,0 +1,13 @@
+public class or35 {
+
+    public boolean or35(int n) {
+        return (n % 3 == 0 || n % 5 == 0);
+    }
+
+    public static void main(String[] args) {
+        or35 obj = new or35();
+        System.out.println(obj.or35(3));   
+        System.out.println(obj.or35(10));
+        System.out.println(obj.or35(8));
+    }
+}
