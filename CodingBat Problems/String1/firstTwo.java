8c30e658732985a423c43be82b7adaa7f5d9fcf4
Omair Al-Falahi <3mairalfala7i33@gmail.com>
2025-07-28 11:03:06 +0400

    string1-firstTwo

---
+++ CodingBat Problems/String1/firstTwo.java
@@ -0,0 +1,14 @@
+public class firstTwo {
+    public static String firstTwo(String str) {
+        if (str.length() < 2) return str;
+        return str.substring(0, 2);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(firstTwo("Hello"));
+        System.out.println(firstTwo("abcdefg"));
+        System.out.println(firstTwo("ab"));
+        System.out.println(firstTwo("X"));
+        System.out.println(firstTwo(""));
+    }
+}
